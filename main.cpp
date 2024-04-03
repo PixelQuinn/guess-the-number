e7be997a162314ec1c69be89d8edbda08e7ce07b
PixelQuinn <quinn.reams@icloud.com>
2024-04-03 16:45:04 -0400

    fix: loop checks fixed to be cleaner with tolower()

--- main.cpp
+++ main.cpp
@@ -6,25 +6,34 @@ using namespace std;
 int main() {
     char gameStart;
 
-    while (gameStart != 'n' && gameStart != 'N' && gameStart != 'y' && gameStart != 'Y') {
+    while (gameStart != 'n' && gameStart != 'y') {
         cout << "Would you like to play my number guessing game? (y or n)" << endl;
         cin >> gameStart;
+        gameStart = tolower(gameStart);
     }
 
-    srand(time(NULL));
+    srand (time(NULL));
     int answer = rand() % 100 + 1;
 
     while(gameStart == 'y' || gameStart == 'Y') {
+
+        char difficulty;
+
+        while (difficulty != 'e' && difficulty != 'm' && difficulty != 'h') {
+            cout << "What difficulty would you like to play on? Easy(input e), Medium(input m), or Hard Mode(input h, if you dare that is!)?" << endl;
+            cin >> difficulty;
+            difficulty = tolower(difficulty);
+        }
 
         int guessedAnswer, numberOfGuesses;
 
         cout << "I am thinking of a number between 1 and 100, take a guess to see if you get it right!" << endl;
         cin >> guessedAnswer;
 
-        while (guessedAnswer < 1 || guessedAnswer > 100 || cin.fail()) {
+        while (guessedAnswer < 1 || guessedAnswer > 100 || cin.fail ()) {
            cout << "This valid is invalid, please try again.." << endl;
-            cin.clear();
-            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cin.clear ();
+            cin.ignore (numeric_limits<streamsize>::max (), '\n');
             cin >> guessedAnswer;
         }
 
@@ -42,6 +51,10 @@ int main() {
             break;
         }
 
+
     }
+
+    system("pause");
+
     return 0;
 }
