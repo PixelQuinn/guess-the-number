e760b8b20271b01c3ecfb6055c7d1d29379f3298
PixelQuinn <quinn.reams@icloud.com>
2024-04-03 16:14:57 -0400

    feat: game logic with a cleansing check on the guessedAnswer added
    core game functional, but more to improve on.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <limits>
+#include <cstdlib>
 using namespace std;
 
 int main() {
@@ -6,12 +8,35 @@ int main() {
 
     cout << "Would you like to play my number guessing game?" << endl;
     cin >> gameStart;
-    cout << endl;
 
-    while(gameStart == 'y' || gameStart == 'Y')
-        int answer = rand() % 100 + 1;
+    srand(time(NULL));
+    int answer = rand() % 100 + 1;
+
+    while(gameStart == 'y' || gameStart == 'Y') {
+
+        int guessedAnswer;
+
         cout << "I am thinking of a number between 1 and 100, take a guess to see if you get it right!" << endl;
+        cin >> guessedAnswer;
 
+        while (guessedAnswer < 1 || guessedAnswer > 100 || cin.fail()) {
+           cout << "This valid is invalid, please try again.." << endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cin >> guessedAnswer;
+        }
 
+        if (guessedAnswer < answer) {
+            cout << "Nope! Your answer is too small." << endl;
+        }
+        else if (guessedAnswer > answer) {
+            cout << "No way! Your number is too big." << endl;
+        }
+        else if (guessedAnswer == answer){
+            cout << "You did it! You found my secret number!" << endl;
+            break;
+        }
+
+    }
     return 0;
 }
